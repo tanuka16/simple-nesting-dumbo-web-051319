a1b023c6fc86dbe479b9d7d9668c241410b797da
Tanuka Das <das.tanuka@yahoo.com>
2019-05-02 20:04:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,13 +64,16 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-	programmer_hash[:yukihiro_matsumoto] = {
+	newly-added-to[:yukihiro_matsumoto] = {
 	 	:known_for => "Ruby",
 		:languages => ["LISP", "C"]
 	 }
 	 newly-added-to
 
-	programmer_hash =
+	programmer_hash[:yukihiro_matsumoto] ={
+	 				:known_for => "Ruby",
+					:languages => ["LISP", "C"]
+	 	}
  		{
         :grace_hopper => {
           :known_for => "COBOL",
