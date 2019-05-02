3b7fbb86c6501739687a5a45481b745ea1ff6e8c
Tanuka Das <das.tanuka@yahoo.com>
2019-05-02 20:16:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,7 +110,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-		 programmer_hash[:alan_kay][:known_for]= alans_new_info
+		 programmer_hash[:alan_kay][:known_for]= alans_new_info							#changes what Alan Kay is known for, returning the newly-changed hash
 		 programmer_hash
 
 
